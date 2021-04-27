b645d8e84791c6f792e00d038bccece3e56df3e2
meight <meightpc@gmail.com>
2021-04-27 21:42:09 +0530

    added array bars

--- src/SortingVisualizer/SortingVisualizer.js
+++ src/SortingVisualizer/SortingVisualizer.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import './SortingVisualizer.css';
 
+
+const NUMBER_OF_ARRAY_BARS = 30;
+const PRIMARY_COLOR = 'pink';
+
+
 export default class SortingVisualizer extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            arr: [1,2,3,4,5],
+            array: [],
         };
+    }
+
+    componentDidMount() {
+        this.resetArray();
+    }
+    
+    resetArray() {
+        function randomIntFromInterval(min, max) {
+            // min and max included
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        const array = [];
+        for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+            array.push(randomIntFromInterval(20, 600));
+        }
+        this.setState({array});
+        // console.log(array);
     }
     
     render() {
 
-        const { arr } = this.state;
+        const { array } = this.state;
 
         return (
             <>
-                <h1>{arr}</h1>
+            <div className="array-container">
+                {array.map((value, idx) => (
+                <div
+                    className="array-bar"
+                    key={idx}
+                    style={{
+                        backgroundColor: PRIMARY_COLOR,
+                        height: `${value}px`,
+                    }}>{value}</div>
+                ))}
+                
+            </div>
+
             </>
         )
     }
