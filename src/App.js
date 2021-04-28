1155147d77ea7e7bc7949c449717b50fac7e0134
meight <meightpc@gmail.com>
2021-04-28 14:02:40 +0530

    added basic sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer'
+import SideBar from './sideBar/sideBar'
+
 
 function App() {
   return (
     <div className="App">
+      <SideBar/>
       <SortingVisualizer/>
     </div>
   );
